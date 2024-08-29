aeed6d6d967b88716d273d1e678bd25ed67acc60
ngari-kev <kevngariwangui@gmail.com>
2024-08-29 18:27:06 +0300

    backend (storage and various classes): implemented storage to fiel and the classes needed

---
+++ backend/models/engine/file_storage.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+"""
+    Serializes and deserializes to and from JSON files respectively.
+"""
+
+from os import path
+import json
+from models.base import Base
+from models.user import User
+from models.skill import Skill
+
+
+class FileStorage():
+    """
+        Serializes and Deserializes instances to and from
+        JSON file respectively
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary of objects.
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with a key.
+
+        Args:
+            obj - Object to be added to __objects.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to a JSON file.
+        """
+        print("Saving objects to JSON...")
+        print(self.__objects)
+        obj_dict = {key: obj.to_dict()
+                      for key, obj in self.__objects.items()}
+        print("saved object")
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects.
+        """
+        if path.exists(self.__file_path):
+            with open(self.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+                for key, object_dict in obj_dict.items():
+                    FileStorage.__objects[key] = eval(
+                        object_dict['__class__'])(**object_dict)
+
+    def count(self, cls=None):
+        """Count the number of instances of a class"""
+        if cls is not None:
+            return sum(1 for instance in self.all().values()
+                       if type(instance).__name__ == cls)
+        return len(self.__objects)
