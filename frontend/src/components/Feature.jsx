8fc842f3db92d9c6dc0294d708901d6602fcd3a1
kyenpya <kyenpyagutap@gmail.com>
2024-09-10 00:26:01 +0100

    Task: minor fixes to the font weight and color of the features cards

--- frontend/src/components/Feature.jsx
+++ frontend/src/components/Feature.jsx
@@ -7,10 +7,10 @@ const Feature = () => {
       {/* Feature Section */}
       <section id="features" className="py-40 bg-gray-50">
         <div className="container mx-auto text-center">
-          <h3 className="text-3xl font-bold mb-10">Project Features</h3>
+          <h3 className="text-3xl text-sky-900 font-bold mb-10">Project Features</h3>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
             {featureCards.map((feature) => (
-              <div key={feature.title} className="bg-white shadow-lg rounded-lg p-6">
+              <div key={feature.title} className="bg-white shadow-xl text-sky-900 rounded-lg p-6">
                 <img
                   src={feature.imageUrl}
                   alt={feature.title}
@@ -19,7 +19,7 @@ const Feature = () => {
                 <h4 className="text-xl font-semibold mb-2">
                   {feature.title}
                 </h4>
-                <p className="text-gray-700">{feature.description}</p>
+                <p className="text-gray-700 text-sky-900">{feature.description}</p>
               </div>
             ))}
           </div>
