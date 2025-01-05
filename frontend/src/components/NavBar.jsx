bf2ea9284deb3379ac17ce3e6f1635e397bed382
Kelvin Mukaria <kevngariwangui@gmail.com>
2025-01-06 00:00:23 +0300

    refining frontend

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
         </Link>
         <Link
           to="/signup"
-          className="ml-4 px-10 py-3 text-white bg-sky-500 font-bold rounded-md hover:bg-sky-700 hover:text-white"
+          className="ml-auto px-10 py-3 text-white bg-sky-500 font-bold rounded-md hover:bg-sky-700 hover:text-white"
         >
           Get started
         </Link>
