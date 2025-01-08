75f832d37cd7547d6aa17e205995b7b0676538c4
Kelvin Mukaria <kevngariwangui@gmail.com>
2025-01-08 13:11:53 +0300

    logging in

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -21,11 +21,14 @@ const Login = () => {
 
     try {
       const res = await fetch("https://skilllinkr.ngarikev.tech/auth/login", {
-        method: "GET",
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        // body: JSON.stringify(form),
+        body: JSON.stringify({
+          username: form.username,
+          password: form.password,
+        }),
       });
 
       console.log(res);
