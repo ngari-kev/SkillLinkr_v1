83c3038ea1e4225d2bce7728231fee0ea18c010c
Kelvin Mukaria <kevngariwangui@gmail.com>
2025-01-08 17:50:50 +0300

    frontend: implements log out feature

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,53 +8,58 @@ const NavBar = () => {
 
   const handleLogout = async () => {
     try {
-      await logoutUser(); // Call the logout API function
-      localStorage.removeItem("access"); // Clear tokens from local storage
+      await logoutUser();
+      localStorage.removeItem("access");
       localStorage.removeItem("refresh");
-      navigate("/login"); // Redirect to login page
+      navigate("/login");
     } catch (error) {
       console.error("Logout failed:", error);
     }
   };
 
   return (
-    <nav className="flex space-x-24">
-      <Link to="/" className="text-sky-900 font-bold hover:text-sky-500 ml-10">
-        Home
-      </Link>
-      <Link to="/about" className="text-sky-900 font-bold hover:text-sky-500">
-        About
-      </Link>
-      <Link
-        to="/marketplace"
-        className="text-sky-900 font-bold hover:text-sky-500"
-      >
-        Marketplace
-      </Link>
-
-      {!isLoggedIn && (
-        <Link to="/login" className="text-sky-900 font-bold hover:text-sky-500">
-          Log in
+    <nav className="flex items-center justify-between px-4 py-2">
+      <div className="flex space-x-8">
+        <Link to="/" className="text-sky-900 font-bold hover:text-sky-500">
+          Home
         </Link>
-      )}
-      {isLoggedIn && (
+        <Link to="/about" className="text-sky-900 font-bold hover:text-sky-500">
+          About
+        </Link>
         <Link
-          to="/profile"
+          to="/marketplace"
           className="text-sky-900 font-bold hover:text-sky-500"
         >
-          Profile
+          Marketplace
         </Link>
-      )}
-      <div className="ml-auto">
         {!isLoggedIn && (
+          <Link
+            to="/login"
+            className="text-sky-900 font-bold hover:text-sky-500"
+          >
+            Log in
+          </Link>
+        )}
+        {isLoggedIn && (
+          <Link
+            to="/profile"
+            className="text-sky-900 font-bold hover:text-sky-500"
+          >
+            Profile
+          </Link>
+        )}
+      </div>
+
+      {/* Right side buttons */}
+      <div>
+        {!isLoggedIn ? (
           <Link
             to="/signup"
             className="px-10 py-3 text-white bg-sky-500 font-bold rounded-md hover:bg-sky-700 hover:text-white"
           >
             Get started
           </Link>
-        )}
-        {isLoggedIn && (
+        ) : (
           <button
             onClick={handleLogout}
             className="px-10 py-3 text-white bg-sky-500 font-bold rounded-md hover:bg-sky-700 hover:text-white"
